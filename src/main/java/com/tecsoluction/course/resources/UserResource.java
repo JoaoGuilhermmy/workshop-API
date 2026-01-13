15332662f185adb223a51b593a26bd4a7d9f6e29
JoaoGuilhermmy <joaoguilhermmy2@gmail.com>
2026-01-13 00:35:04 -0300

    User entity and resourse

---
+++ src/main/java/com/tecsoluction/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.tecsoluction.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tecsoluction.course.entities.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findall() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
